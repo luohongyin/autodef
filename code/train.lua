9ecaa37ee9409f3387e376bb1a683b3f6969f067
skTim <lhythu@gmail.com>
2016-03-27 15:47:15 +0800

    commit on NEXT GPU server

--- code/train.lua
+++ code/train.lua
@@ -9,7 +9,7 @@ cmd:option('--cuda', false, 'use CUDA')
 cmd:option('--hiddenSize', 1000, 'number of hidden units in LSTM')
 cmd:option('--learningRate', 0.05, 'learning rate at t=0')
 cmd:option('--momentum', 0.9, 'momentum')
-cmd:option('--minLR', 0.00025, 'minimum learning rate')
+cmd:option('--minLR', 0.000025, 'minimum learning rate')
 cmd:option('--saturateEpoch', 40, 'epoch at which linear decayed LR will reach minLR')
 cmd:option('--maxEpoch', 50, 'maximum number of epochs to run')
 cmd:option('--batchSize', 400, 'number of examples to load at once')
@@ -93,7 +93,7 @@ for epoch = 1, options.maxEpoch do
 
         errors[i] = err
 	    end
-      -- xlua.progress(i, dataset.examplesCount)
+      xlua.progress(i, dataset.examplesCount)
       i = i + 1
     end
   end
