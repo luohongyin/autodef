1718bb73373176a110a81d5892743e5e45bb8435
sktim <lhythu@gmail.com>
2016-05-24 22:41:22 +0800

    commit on MAC

--- code/lookup.lua
+++ code/lookup.lua
@@ -5,30 +5,14 @@ function Lookup:__init(route)
 end
 
 function Lookup:updateOutput(input)
-   self.output = self.weight:index(1, torch.LongTensor(input))
+   self.output = self.weight:index(1, torch.LongTensor(torch.totable(input)))
    return self.output
 end
 
 function Lookup:updateGradInput(input, gradOutput)
-   if self.inplace then
-      self.gradInput:set(gradOutput)
-   else
-      self.gradInput:resizeAs(gradOutput):copy(gradOutput)
-   end
    return self.gradInput
 end
 
-function Lookup:setp(p)
-   self.p = p
-end
-
-function Lookup:__tostring__()
-   return string.format('%s(%f)', torch.type(self), self.p)
-end
-
 function Lookup:clearState()
-   if self.noise then
-      self.noise:set()
-   end
    return Parent.clearState(self)
 end
