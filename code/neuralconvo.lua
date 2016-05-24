a51316c49c4f483fc98e64a98b95932d8bdacd2b
sktim <lhythu@gmail.com>
2016-05-24 22:48:06 +0800

    commit on MAC

--- code/neuralconvo.lua
+++ code/neuralconvo.lua
@@ -3,12 +3,12 @@ require 'nn'
 require 'rnn'
 
 neuralconvo = {}
-
 torch.include('neuralconvo', 'wordnet.lua')
 torch.include('neuralconvo', 'dataset.lua')
 torch.include('neuralconvo', 'AdaSeq2seq.lua')
 torch.include('neuralconvo', 'sampling.lua')
 torch.include('neuralconvo', 'seq2seq.lua')
 torch.include('neuralconvo', 'AdaLSTM.lua')
+torch.include('neuralconvo', 'lookup.lua')
 
 return neuralconvo
